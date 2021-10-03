6e8c0215725f8ec5e01bddc942f6bf3cb23270c8
Zachintosh <zlw11063@gmail.com>
2021-10-02 19:57:51 -0600

    Update vite config to properly build assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import linaria from 'vite-plugin-linaria-styled'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/lunar-phases/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   plugins: [
     react(),
     linaria({
